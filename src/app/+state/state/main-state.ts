37a6026237fcb5b4209c9d3c4abdace3ae44454d
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-11-23 09:20:46 +0200

    initial state has been modified

--- src/app/+state/state/main-state.ts
+++ src/app/+state/state/main-state.ts
@@ -2,12 +2,12 @@ import { Task } from './../../models/task';
 
 export interface State {
   tasks: Array<Task>;
-  pending: boolean;
-  error: any;
 }
 
 export const intitialState: State = {
-  tasks: [],
-  pending: false,
-  error: null
+  tasks: [
+    new Task(1, 'Estimate', 1, 8, 8, true),
+    new Task(2, 'Create', 2, 8, 4, false),
+    new Task(3, 'Deploy', 3, 8, 0, false)
+  ]
 };
