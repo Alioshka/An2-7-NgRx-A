f47090d8c31b705b462f3544ecc27d06772e5b82
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-12-06 12:34:09 +0200

    app state has been devided into tasks and app state

--- src/app/+state/state/tasks.state.ts
+++ src/app/+state/state/tasks.state.ts
@@ -1,11 +1,11 @@
 import { Task } from './../../models/task';
 
-export interface State {
-  tasks: Array<Task>;
+export interface TasksState {
+  data: Array<Task>;
 }
 
-export const intitialState: State = {
-  tasks: [
+export const intitialState: TasksState = {
+  data: [
     new Task(1, 'Estimate', 1, 8, 8, true),
     new Task(2, 'Create', 2, 8, 4, false),
     new Task(3, 'Deploy', 3, 8, 0, false)
