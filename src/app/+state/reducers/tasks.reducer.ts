d0948f0ba75ad1992f92c94a3f4316ebbdc2c39d
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-12-06 13:19:46 +0200

    app reducer hase been added

--- src/app/+state/reducers/tasks.reducer.ts
+++ src/app/+state/reducers/tasks.reducer.ts
@@ -1,28 +1,28 @@
 import { TasksActionTypes, TasksActions } from './../actions/tasks.actions';
-import { State, intitialState } from '../state/main-state';
+import { TasksState, intitialState } from '../state/tasks.state';
 
-export function reducer( state = intitialState, action: TasksActions ): State {
+export function reducer( state = intitialState, action: TasksActions ): TasksState {
   console.log(`Reducer: Action came in! ${action.type}`);
 
   switch (action.type) {
     case TasksActionTypes.GET_TASKS: {
       console.log('GET_TASKS action being handled!');
-      return Object.assign({}, state);
+      return {...state};
     }
 
     case TasksActionTypes.CREATE_TASK: {
       console.log('CREATE_TASK action being handled!');
-      return Object.assign({}, state);
+      return {...state};
     }
 
     case TasksActionTypes.UPDATE_TASK: {
       console.log('UPDATE_TASK action being handled!');
-      return Object.assign({}, state);
+      return {...state};
     }
 
     case TasksActionTypes.DELETE_TASK: {
       console.log('DELETE_TASK action being handled!');
-      return Object.assign({}, state);
+      return {...state};
     }
 
     default: {
--- src/app/+state/state/app.state.ts
+++ src/app/+state/state/app.state.ts
@@ -1,6 +1,13 @@
+import { ActionReducerMap } from '@ngrx/store';
+
 import { TasksState } from './tasks.state';
+import * as fromTasks from './../reducers/tasks.reducer';
 
 export interface AppState {
   tasks: TasksState;
 }
+
+export const reducers: ActionReducerMap<AppState> = {
+  tasks: fromTasks.reducer
+};
 
