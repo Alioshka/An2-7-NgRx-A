3a09661fc088252f88469c98d0567091f1fdf9d7
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-12-28 14:15:22 +0200

    @ngrx/entity has been added

--- src/app/+store/selectors/tasks.selectors.ts
+++ src/app/+store/selectors/tasks.selectors.ts
@@ -1,23 +1,36 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-import { TasksState } from './../state';
+import { taskAdapter, TasksState } from './../state';
 import { getRouterState } from './../selectors/router.selectors';
 import { Task } from './../../models/task';
 
 
 export const getTasksState = createFeatureSelector<TasksState>('tasks');
 
-export const getTasksData = createSelector(getTasksState, (state: TasksState) => state.data);
+export const {
+    selectEntities: getTasksEntities,
+    selectAll: getTasksData } = taskAdapter.getSelectors(getTasksState);
 export const getTasksError = createSelector(getTasksState, (state: TasksState) => state.error);
 export const getSelectedTask = createSelector(getTasksState, (state: TasksState) => state.selectedTask);
 export const getTasksLoaded = createSelector(getTasksState, (state: TasksState) => state.loaded);
 
+// export const getSelectedTaskByUrl = createSelector(
+//     getTasksData,
+//     getRouterState,
+//     (tasks, router): Task => {
+//         if (router.state.params.id) {
+//             return tasks.find(task => task.id === +router.state.params.id);
+//         } else {
+//             return new Task(null, '', null, null);
+//         }
+// });
+
 export const getSelectedTaskByUrl = createSelector(
-    getTasksData,
+    getTasksEntities,
     getRouterState,
     (tasks, router): Task => {
         if (router.state.params.id) {
-            return tasks.find(task => task.id === +router.state.params.id);
+            return tasks[router.state.params.id];
         } else {
             return new Task(null, '', null, null);
         }
--- src/app/+store/state/tasks.state.ts
+++ src/app/+store/state/tasks.state.ts
@@ -1,19 +1,22 @@
+import { createEntityAdapter, EntityState, EntityAdapter } from '@ngrx/entity';
+
 import { Task } from './../../models/task';
 
-export interface TasksState {
-  data: ReadonlyArray<Task>;
+export interface TasksState extends EntityState<Task> {
   selectedTask: Readonly<Task>;
   readonly loading: boolean;
   readonly loaded: boolean;
   readonly error: Error | string;
 }
 
-export const intitialTasksState: TasksState = {
-  data: [],
+export const taskAdapter: EntityAdapter<Task> = createEntityAdapter<Task>();
+
+export const initialTasksState: TasksState = taskAdapter.getInitialState({
+  // additional entity state properties
   selectedTask: null,
   loading: false,
   loaded: false,
   error: null
-};
+});
 
 
