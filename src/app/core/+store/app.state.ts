554064b550a037797cbf850a6efd1b3913e3d31b
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-03-05 17:21:14 +0200

    fix4

--- src/app/core/+store/app.state.ts
+++ src/app/core/+store/app.state.ts
@@ -5,8 +5,3 @@ import { TasksState, tasksReducer } from './tasks';
 export interface AppState {
   tasks: TasksState;
 }
-
-export const reducers: ActionReducerMap<AppState> = {
-  tasks: tasksReducer
-};
-
