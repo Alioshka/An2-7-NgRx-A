6170c51e8de268060bcd9aed33a8481e09179b1e
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-03-14 11:53:19 +0200

    fix20

--- src/app/core/+store/core-store.module.ts
+++ src/app/core/+store/core-store.module.ts
@@ -5,15 +5,23 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { RouterStateSerializerProvider, routerReducers } from '.';
+
 import { environment } from './../../../environments/environment';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(routerReducers),
+    StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([]),
+
     // Instrumentation must be imported after importing StoreModule (config is optional)
     !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
+  providers: [
+    RouterStateSerializerProvider,
   ],
   declarations: []
 })
